ee2adfb6878c4f971acee7e6d93709e252906c61
Matthew Wo <9029537@gmail.com>
2017-12-02 17:05:26 -0800

    add nginx and test API

--- server/index.js
+++ server/index.js
@@ -3,4 +3,9 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/people', function (req, res) {
+	console.log(req.query)
+	res.json([{"1":"a"},{"2":"b"}]);
+})
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
